8838e131106de0b84f07a53a79292124ec8e98ee
Raul <raul.sequeira96@gmail.com>
2020-01-04 21:48:51 -0300

    Base del proyecto, se agrega el componente details y results

---
+++ src/components/details/Details.js
@@ -0,0 +1,19 @@
+import React, { Fragment, Component } from 'react';
+
+function Page(props){
+    return (
+        <Fragment>
+            Details
+        </Fragment>
+    );
+}
+
+class Details extends Component{
+    render(){
+        return(
+            <Page />
+        );
+    }
+}
+
+export default Details;
---
+++ src/components/results/Results.js
@@ -0,0 +1,19 @@
+import React, { Fragment, Component } from 'react';
+
+function Page(props){
+    return (
+        <Fragment>
+            Results
+        </Fragment>
+    );
+}
+
+class Results extends Component{
+    render(){
+        return(
+            <Page />
+        );
+    }
+}
+
+export default Results;
